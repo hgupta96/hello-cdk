2455e8118923f2c062c5a88bbec1aafb8ed7a604
Hardik Gupta <hardikgupta@hardiks-mbp.mynetworksettings.com>
2023-02-21 09:12:49 -0500

    Git repo for lambda initialized

---
+++ backend/lambda/post.ts
@@ -0,0 +1,11 @@
+import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+
+
+export const handler = async(event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+    console.log("Post Requested",event)
+    return{
+        body : JSON.stringify(event.body),
+        statusCode : 200
+    }
+}
+
--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,13 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { ApiGateway } from './ApiGateway';
+import { Lambda } from './Lambda';
+
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const api = new ApiGateway(this);
     // The code that defines your stack goes here
-
+    const healthLambda = new Lambda(this, "health");
+    const postLambda = new Lambda(this, "post");
+    api.getIntegration("GET","/health",healthLambda);
+    api.postIntegration("POST","/post",postLambda);
     // example resource
     // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
